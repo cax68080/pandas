2fe0c4877023346a75fffa09fd5c69e6d4b64525
cax68080 <cax68080@pop06.odn.ne.jp>
2024-03-25 23:57:00 +0900

    2024/3/25

--- ライブラリ活用入門/sample001.py
+++ ライブラリ活用入門/sample001.py
@@ -1,5 +1,9 @@
 import pandas as pd
 df = pd.read_csv("./gapminder.tsv",sep="\t")
-print(df)
-print(df.shape)
-print(df.columns)
+#print(df)
+#print(df.shape)
+#print(type(df))
+#print(df.columns)
+#print(df.dtypes)
+#print(df.info())
+print(df.head())
